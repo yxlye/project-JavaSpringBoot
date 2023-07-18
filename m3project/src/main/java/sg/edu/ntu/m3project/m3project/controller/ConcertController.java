9c41e572784b5e65840f06cb0bcb31d295d288de
yxlye <lye.yong.xin.9660@gmail.com>
2023-07-18 22:55:01 +0800

    updated concert entity

--- m3project/src/main/java/sg/edu/ntu/m3project/m3project/controller/ConcertController.java
+++ m3project/src/main/java/sg/edu/ntu/m3project/m3project/controller/ConcertController.java
@@ -1,6 +1,9 @@
 package sg.edu.ntu.m3project.m3project.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,38 +25,46 @@ public class ConcertController {
     @Autowired
     ConcertService concertService;
 
+    List<ConcertEntity> concertList;
+    ConcertEntity selectedConcert;
+
     @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity<?> findAllAvailable() {
-        return concertService.find("upcoming", "");
+        concertList = (List<ConcertEntity>) concertService.find("upcoming", "");
+        return new ResponseEntity<List<ConcertEntity>>(concertList, HttpStatus.OK);
     }
 
     @RequestMapping(value = "/history", method = RequestMethod.GET)
     public ResponseEntity<?> findAll() {
-        return concertService.find("history", "");
-
+        concertList = concertService.find("history", "");
+        return new ResponseEntity<List<ConcertEntity>>(concertList, HttpStatus.OK);
     }
 
     @RequestMapping(value = "/search", method = RequestMethod.GET)
     public ResponseEntity<?> search(@RequestParam String artist) {
 
-        return concertService.find("artist", artist.toUpperCase());
+        concertList = concertService.find("artist", artist.toUpperCase());
+        return new ResponseEntity<List<ConcertEntity>>(concertList, HttpStatus.OK);
     }
 
     @RequestMapping(value = "/search/{concertId}", method = RequestMethod.GET)
     public ResponseEntity<?> findById(@PathVariable int concertId) {
-        return concertService.findbyConcertId(concertId);
+        selectedConcert = concertService.findbyConcertId(concertId);
+        return new ResponseEntity<ConcertEntity>(selectedConcert, HttpStatus.OK);
     }
 
     @RequestMapping(value = "/edit", method = RequestMethod.POST)
     public ResponseEntity<?> create(@RequestHeader("token") String token, @RequestHeader("user-id") int userId,
             @RequestBody ConcertEntity concert) {
-        return concertService.create(token, userId, concert);
+        selectedConcert = concertService.create(token, userId, concert);
+        return new ResponseEntity<ConcertEntity>(selectedConcert, HttpStatus.CREATED);
     }
 
     @RequestMapping(value = "/edit/{concertId}", method = RequestMethod.PUT)
     public ResponseEntity<?> update(@RequestHeader("user-id") int userId, @RequestBody ConcertEntity concert,
             @PathVariable int concertId) {
-        return concertService.update(userId, concert, concertId);
+        selectedConcert = concertService.update(userId, concert, concertId);
+        return new ResponseEntity<ConcertEntity>(selectedConcert, HttpStatus.OK);
     }
 
 }
--- m3project/src/main/java/sg/edu/ntu/m3project/m3project/service/ConcertService.java
+++ m3project/src/main/java/sg/edu/ntu/m3project/m3project/service/ConcertService.java
@@ -1,18 +1,15 @@
 package sg.edu.ntu.m3project.m3project.service;
 
-import java.nio.file.AccessDeniedException;
 import java.sql.Timestamp;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import sg.edu.ntu.m3project.m3project.entity.ConcertEntity;
-import sg.edu.ntu.m3project.m3project.helper.ResponseMessage;
+import sg.edu.ntu.m3project.m3project.exceptions.ConcertNotFoundException;
 import sg.edu.ntu.m3project.m3project.repository.ConcertRepository;
 import sg.edu.ntu.m3project.m3project.repository.UserRepository;
 
@@ -28,7 +25,7 @@ public class ConcertService {
     @Autowired
     UserService userService;
 
-    public ResponseEntity<?> find(String findBy, String searchParam) {
+    public List<ConcertEntity> find(String findBy, String searchParam) {
 
         List<ConcertEntity> currentConcertList;
 
@@ -60,31 +57,36 @@ public class ConcertService {
 
         if (currentConcertList.size() > 0) {
 
-            return ResponseEntity.ok().body(currentConcertList);
+            return currentConcertList;
         }
 
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ResponseMessage("No upcoming concerts"));
+        throw new ConcertNotFoundException("No upcoming concerts");
     }
 
-    public ResponseEntity<?> findbyConcertId(int concertId) {
+    public ConcertEntity findbyConcertId(int concertId) {
 
         Optional<ConcertEntity> optionalConcert = concertRepo.findById(concertId);
 
         if (optionalConcert.isPresent()) {
-            ConcertEntity selectedConcert = optionalConcert.get();
-            return new ResponseEntity<ConcertEntity>(selectedConcert, HttpStatus.OK);
+            return optionalConcert.get();
+
         }
 
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ResponseMessage("Invalid concert id."));
+        throw new ConcertNotFoundException("Invalid concert id.");
     }
 
-    public ResponseEntity<?> create(String token, int userId, ConcertEntity concert) {
+    public ConcertEntity create(String token, int userId, ConcertEntity concert) {
         userService.checkAdmin(userId);
         ConcertEntity newConcert = concertRepo.save(concert);
-        return new ResponseEntity<>(concertRepo.findById(newConcert.getId()), HttpStatus.CREATED);
+
+        Optional<ConcertEntity> optionalConcert = concertRepo.findById(newConcert.getId());
+        if (optionalConcert.isPresent()) {
+            return optionalConcert.get();
+        }
+        throw new ConcertNotFoundException("Error encountered to retrieve concert created");
     }
 
-    public ResponseEntity<?> update(int userId, ConcertEntity concert, int concertId) {
+    public ConcertEntity update(int userId, ConcertEntity concert, int concertId) {
         userService.checkAdmin(userId);
         Optional<ConcertEntity> optionalConcert = concertRepo.findById(concertId);
 
@@ -100,10 +102,10 @@ public class ConcertService {
             selectedConcert.setUpdatedAt(updatedAt);
 
             concertRepo.save(selectedConcert);
-            return ResponseEntity.ok().body(selectedConcert);
+            return selectedConcert;
         }
 
-        return ResponseEntity.notFound().build();
+        throw new ConcertNotFoundException("Concert to be edited does not exist");
 
     }
 
