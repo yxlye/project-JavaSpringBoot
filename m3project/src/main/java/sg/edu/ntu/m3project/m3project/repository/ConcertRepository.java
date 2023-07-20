d235b756f6dc16bd1bea70a364d7375204018094
yxlye <lye.yong.xin.9660@gmail.com>
2023-07-20 12:10:29 +0800

    updated repository

--- m3project/src/main/java/sg/edu/ntu/m3project/m3project/repository/ConcertRepository.java
+++ m3project/src/main/java/sg/edu/ntu/m3project/m3project/repository/ConcertRepository.java
@@ -4,9 +4,11 @@ import java.sql.Timestamp;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import sg.edu.ntu.m3project.m3project.entity.ConcertEntity;
 
+@Repository
 public interface ConcertRepository extends CrudRepository<ConcertEntity, Integer> {
     List<ConcertEntity> findByConcertDateAfter(Timestamp concertDate);
 
